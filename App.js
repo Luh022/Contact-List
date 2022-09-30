86dba0406a019d1496c5d607c63796743ea7d893
Luana Roza <102182950+Luh022@users.noreply.github.com>
2022-09-30 17:01:35 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,80 @@
+import {
+    Alert,
+    FlatList,
+    Keyboard,
+    StyleSheet,
+    TouchableWithoutFeedback,
+    View,
+  } from 'react-native';
+  import React, { useState } from 'react';
+ 
+  import AddTodo from './components/addTodo';
+  import Header from './components/header';
+  import TodoItem from './components/todoItem';
+
+  
+  export default function App() {
+    const [todos, setTodos] = useState([
+      { id: 1, text: 'Adicionar novo contato' },
+      { id: 2, text: 'Adicionar nome email' },
+      { id: 3, text: 'Adicionar data de nascimento' },
+    ]);
+
+    const pressHandler = id => {
+      setTodos(prevTodos => {
+        return prevTodos.filter(todo => todo.id !== id);
+      });
+    };
+  
+    const submitHandler = text => {
+      if (text.length > 3) {
+        setTodos(prevTodos => {
+          return [...prevTodos, { text, id: todos.length + 1 }];
+        });
+      } else {
+        Alert.alert(
+          'Deu ruim!',
+          'As tarefas precisam ter no mínimo 3 caracteres.',
+          [{ text: 'Entendi', onPress: () => console.log('Alerta fechado.') }]
+        );
+      }
+    };
+  
+    return (
+      <TouchableWithoutFeedback
+        onPress={() => {
+          Keyboard.dismiss();
+          console.log('Saí teclado!');
+        }}
+      >
+        <View style={styles.container}>
+          <Header />
+          <View style={styles.content}>
+            <AddTodo submitHandler={submitHandler} />
+            <View style={styles.list}>
+              <FlatList
+                data={todos}
+                keyExtractor={todo => todo.id}
+                renderItem={({ item }) => (
+                  <TodoItem item={item} onPress={pressHandler} />
+                )}
+              />
+            </View>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+    },
+    content: {
+      flex: 1,
+      padding: 40,
+    },
+    list: { flex: 1, marginTop: 20 },
+  });
+        
