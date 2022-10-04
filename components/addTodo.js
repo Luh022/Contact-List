dd85821122da594a3c27453ef6823ec3a0ba2aa2
Luana Roza <102182950+Luh022@users.noreply.github.com>
2022-10-04 15:31:49 -0300

    Add files via upload

--- components/addTodo.js
+++ components/addTodo.js
@@ -12,7 +12,7 @@ export default function AddTodo({ submitHandler }) {
     <View>
       <TextInput
         style={styles.input}
-        placeholder="Nova tarefa..."
+        placeholder="Novo contato..."
         onChangeText={changeHandler}
         // onChangeText={val => changeHandler(val)}
       />
